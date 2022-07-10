6729bed89128d085fbe6def737756548c0c58e55
DmitriyGovosyants <Demiangvs@gmail.com>
2022-07-10 12:03:57 +0300

    fix: data transfer to data directiory

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,10 +4,10 @@ import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 
-import user from 'components/Profile/user';
-import data from 'components/Statistics/data';
-import friends from 'components/FriendList/friends';
-import transactions from 'components/TransactionHistory/transactions';
+import user from 'data/user.json';
+import data from 'data/data.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
 
 export const App = () => {
   return (
